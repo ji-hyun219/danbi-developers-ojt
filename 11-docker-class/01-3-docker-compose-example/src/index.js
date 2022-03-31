cea3c6d1a50fd5d8bbd0b3bb9b5840a70148cb0c
Eunchurn Park <eunchurn.park@gmail.com>
2022-03-31 16:56:30 +0900

    update

--- 11-docker-class/01-3-docker-compose-example/src/index.js
+++ 11-docker-class/01-3-docker-compose-example/src/index.js
@@ -8,7 +8,7 @@ const server = http
     console.log(`${method}: ${url}`)
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json");
-    response.end(JSON.stringify({ hello: "world" }));
+    response.end(JSON.stringify({ hello: "world2" }));
   })
   .listen(port, () => {
     console.log(`Listening http://0.0.0.0:${port}`)
